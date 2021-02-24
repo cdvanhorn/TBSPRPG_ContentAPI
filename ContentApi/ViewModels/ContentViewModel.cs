7c5c375ffc3ddeb1d9f7e12f0125f1067c34d2e2
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-02-23 21:19:04 -0500

    ContentAPI
    Added the current index in the stream of the event.

--- ContentApi/ViewModels/ContentViewModel.cs
+++ ContentApi/ViewModels/ContentViewModel.cs
@@ -7,10 +7,13 @@ namespace ContentApi.ViewModels {
         public ContentViewModel(ContentAggregate agg) {
             Id = agg.Id;
             Texts = agg.Text;
+            Index = agg.StreamPosition;
         }
 
         public string Id { get; set; }
 
         public List<string> Texts { get; set; }
+
+        public ulong Index { get; set; }
     }
 }
