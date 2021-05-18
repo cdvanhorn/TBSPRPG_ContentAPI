8ec03eb9aeec997658331e96847cdfd066c0a575
cdvanhorn <cdavid.vanhorn@gmail.com>
2021-05-17 20:49:35 -0400

    TbspRpg ContentApi
    Updated new game handler tests.

--- ContentApi.Tests/EventProcessors/NewGameEventHandlerTests.cs
+++ ContentApi.Tests/EventProcessors/NewGameEventHandlerTests.cs
@@ -7,6 +7,7 @@ using ContentApi.Repositories;
 using ContentApi.Services;
 using TbspRpgLib.Aggregates;
 using TbspRpgLib.Events;
+using TbspRpgLib.Events.Game;
 using Xunit;
 
 namespace ContentApi.Tests.EventProcessors
@@ -31,7 +32,16 @@ namespace ContentApi.Tests.EventProcessors
                 Id = _testGameId
             };
 
+            var content = new Content()
+            {
+                GameId = _testGameId,
+                Id = Guid.NewGuid(),
+                Position = 42,
+                Text = "game created"
+            };
+
             context.Games.Add(game);
+            context.Contents.Add(content);
             context.SaveChanges();
         }
         
@@ -62,18 +72,25 @@ namespace ContentApi.Tests.EventProcessors
                 Id = Guid.NewGuid().ToString(),
                 AdventureId = Guid.NewGuid().ToString()
             };
+
+            var evnt = new GameNewEvent()
+            {
+                EventId = Guid.NewGuid(),
+                StreamPosition = 43
+            };
             
             //act
-            await handler.HandleEvent(agg, null);
+            await handler.HandleEvent(agg, evnt);
             
             //assert
             context.SaveChanges();
             //there should be a new game in the database
             Assert.NotNull(context.Games.FirstOrDefault(g => g.Id.ToString() == agg.Id));
+            Assert.NotNull(context.Contents.FirstOrDefault(c => c.GameId.ToString() == agg.Id));
         }
         
         [Fact]
-        public async void HandleEvent_Exists_GameNotCreated()
+        public async void HandleEvent_GameExists_GameNotCreatedContentCreated()
         {
             //arrange
             await using var context = new ContentContext(_dbContextOptions);
@@ -84,13 +101,48 @@ namespace ContentApi.Tests.EventProcessors
                 AdventureId = Guid.NewGuid().ToString()
             };
             
+            var evnt = new GameNewEvent()
+            {
+                EventId = Guid.NewGuid(),
+                StreamPosition = 43
+            };
+            
             //act
-            await handler.HandleEvent(agg, null);
+            await handler.HandleEvent(agg, evnt);
             
             //assert
             context.SaveChanges();
             //there should be a new game in the database
             Assert.Single(context.Games.AsQueryable().Where(g => g.Id == _testGameId));
+            Assert.Equal(2, context.Contents.AsQueryable().Count(c => c.GameId.ToString() == agg.Id));
+        }
+        
+        [Fact]
+        public async void HandleEvent_GameContentExists_GameContentNotCreated()
+        {
+            //arrange
+            await using var context = new ContentContext(_dbContextOptions);
+            var handler = CreateHandler(context);
+            var agg = new GameAggregate()
+            {
+                Id = _testGameId.ToString(),
+                AdventureId = Guid.NewGuid().ToString()
+            };
+            
+            var evnt = new GameNewEvent()
+            {
+                EventId = Guid.NewGuid(),
+                StreamPosition = 42
+            };
+            
+            //act
+            await handler.HandleEvent(agg, evnt);
+            
+            //assert
+            context.SaveChanges();
+            //there should be a new game in the database
+            Assert.Single(context.Games.AsQueryable().Where(g => g.Id == _testGameId));
+            Assert.Single(context.Contents.AsQueryable().Where(c => c.GameId.ToString() == agg.Id));
         }
 
         #endregion
